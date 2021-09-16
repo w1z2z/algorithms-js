d1aef964b7b88423b6f9f5b8a8160d20d0d00932
Vitaly Kochergin <vitalya.kochergin@gmail.com>
2021-09-17 01:32:36 +0300

    added the code of the recursive binary search algorithm

--- binarySearch.js
+++ binarySearch.js
@@ -3,6 +3,7 @@
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let count = 0; 
 
+//  Без рекурсии -
 function binarySearch(arr, item) {
     let low = 0; 
     let high = arr.length - 1;
@@ -10,7 +11,7 @@ function binarySearch(arr, item) {
         count++;
         let mid = Math.floor((low + high) / 2);
         guess = arr[mid];
-        console.log(guess);
+        // console.log(guess);
         if (guess == item) {
             return mid;
         } else if (guess > item) {
@@ -22,7 +23,22 @@ function binarySearch(arr, item) {
     return 'Данного элемента нет в массиве!'
 }
 
+//  С рекурсией +
+function recursiveBinarySearch(arr, item, low, high) {
+    let mid = Math.floor((low + high) / 2);
+    count++;
+    if(item === arr[mid]) {
+        return mid;
+    }
+    if(item < arr[mid]) {
+        return recursiveBinarySearch(arr, item, low, mid - 1);
+    } else {
+        return recursiveBinarySearch(arr, item, mid + 1, high);
+    }
+}
+
 const num = 4;
 console.log('Искомый элемент: ', num);
-console.log('Индекс искомого элемента: ', binarySearch(arr, num));
+// console.log('Индекс искомого элемента: ', binarySearch(arr, num));
+console.log('Индекс искомого элемента: ', recursiveBinarySearch(arr, num, 0, arr.length));
 console.log('Количество итераций: ', count);
